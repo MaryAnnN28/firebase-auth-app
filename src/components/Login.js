cf0cde3a0cb22caa7ea4116bce6f2636489806d7
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-03-15 20:20:32 -0400

    added router to sign up and login pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   const emailRef = useRef()
@@ -44,7 +45,7 @@ export default function Login() {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? Log In 
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   )
